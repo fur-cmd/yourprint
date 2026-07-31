6c2dc0096657517b467052b3182362a646606b7e
Yunus Usman <usmanyunus362@gmail.com>
2026-07-31 14:02:46 +0800

    Buat  produk digital dapa di scroll di hp

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 window.YOURPRINT_CONFIG = {
-  GAS_URL: "https://script.google.com/macros/s/AKfycbyT4smOl16f5GVsfUktreua1f6qw3D55H5XjNnlTUvhEojaMvk-dmFhZgt6OoYYVNKt/exec",
+  GAS_URL: "https://script.google.com/macros/s/AKfycbyleUrvtjr7-SaYfv9zx8o63LXegOwNXu6yvkAuBeeJ5yH-DAnqlpn7BBNVGU1lDpXl/exec",
   WHATSAPP_NUMBER: "6285242410880",
   UNDANGAN_DIGITAL_URL: "https://undangan.yourprint.id",
   /* Halaman pembayaran Market Digital di Lynk.id.
