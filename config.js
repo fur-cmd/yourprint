2e014301658b8d5f9cf45462bb26513618d13880
Yunus Usman <usmanyunus362@gmail.com>
2026-08-23 07:49:40 +0800

    Perbaikan domain dikode dan pembuatan google console

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 window.YOURPRINT_CONFIG = {
   GAS_URL: "https://script.google.com/macros/s/AKfycbzfeWkKbfUJ-fqW_vwKuJzRqlcdsclioltMAoXg-iOc5uRIeoqSZ10821dKHhjlDO8b/exec",
   WHATSAPP_NUMBER: "6285242410880",
-  UNDANGAN_DIGITAL_URL: "https://undangan.yourprint.id",
+  UNDANGAN_DIGITAL_URL: "https://undangan.yourprint.my.id",
   /* Halaman pembayaran Market Digital di Lynk.id.
      GANTI URL ini jika halaman Lynk.id berubah. */
   DIGITAL_LYNK_URL: "https://lynk.id/market.digital123",
